86234ce31f67fcc5ff4ed492750ac53c0742a223
Risky Armansyah <rarmnsyah787@gmail.com>
2023-03-02 20:46:02 +0700

    Update test.py

--- image-processing/pengolahan-citra/test.py
+++ image-processing/pengolahan-citra/test.py
@@ -1,27 +1,10 @@
-pattern = "abbab"
-s = "dog cat cat dog dog"
-s = s.split(' ')
-
-# print(s, pattern.split(''))
-def anjay(pattern, s):
-    if(len(pattern) != len(s)): return (False)
-
-    temp = {}
-
-    for i, j in zip(pattern, s):
-        if i in temp:
-            if temp[i] != j:
-                return False
-        elif j in temp.values():
-            return False
-        else:
-            temp[i] = j
-    return True
-
-print(anjay(pattern, s))
-        
-
-
-
-
-
+output = []
+n = 2
+for i in range(n+1):
+    temp = 0
+    while(i>0):
+        temp += 1 if i%2 == 1 else 0
+        i = i//2
+    print (i, temp, output)
+    output.append(temp)
+print(output)
