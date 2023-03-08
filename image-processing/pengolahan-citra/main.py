da28425396923b1d7cd6f7f68f93db7b90794210
Risky Armansyah <rarmnsyah787@gmail.com>
2023-03-08 11:05:27 +0700

    Update main.py

--- image-processing/pengolahan-citra/main.py
+++ image-processing/pengolahan-citra/main.py
@@ -125,6 +125,97 @@ def img_freq_hist(image):
     plt.bar(freq_hist.keys(), freq_hist.values())
     plt.show()
 
+def Canny_detector(img, weak_th = None, strong_th = None):
+      
+    # conversion of image to grayscale
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+       
+    # Noise reduction step
+    img = cv2.GaussianBlur(img, (5, 5), 1.4)
+       
+    # Calculating the gradients
+    gx = cv2.Sobel(np.float32(img), cv2.CV_64F, 1, 0, 3)
+    gy = cv2.Sobel(np.float32(img), cv2.CV_64F, 0, 1, 3)
+      
+    # Conversion of Cartesian coordinates to polar 
+    mag, ang = cv2.cartToPolar(gx, gy, angleInDegrees = True)
+       
+    # setting the minimum and maximum thresholds 
+    # for double thresholding
+    mag_max = np.max(mag)
+    if not weak_th:weak_th = mag_max * 0.1
+    if not strong_th:strong_th = mag_max * 0.5
+      
+    # getting the dimensions of the input image  
+    height, width = img.shape
+       
+    # Looping through every pixel of the grayscale 
+    # image
+    for i_x in range(width):
+        for i_y in range(height):
+               
+            grad_ang = ang[i_y, i_x]
+            grad_ang = abs(grad_ang-180) if abs(grad_ang)>180 else abs(grad_ang)
+               
+            # selecting the neighbours of the target pixel
+            # according to the gradient direction
+            # In the x axis direction
+            if grad_ang<= 22.5:
+                neighb_1_x, neighb_1_y = i_x-1, i_y
+                neighb_2_x, neighb_2_y = i_x + 1, i_y
+              
+            # top right (diagonal-1) direction
+            elif grad_ang>22.5 and grad_ang<=(22.5 + 45):
+                neighb_1_x, neighb_1_y = i_x-1, i_y-1
+                neighb_2_x, neighb_2_y = i_x + 1, i_y + 1
+              
+            # In y-axis direction
+            elif grad_ang>(22.5 + 45) and grad_ang<=(22.5 + 90):
+                neighb_1_x, neighb_1_y = i_x, i_y-1
+                neighb_2_x, neighb_2_y = i_x, i_y + 1
+              
+            # top left (diagonal-2) direction
+            elif grad_ang>(22.5 + 90) and grad_ang<=(22.5 + 135):
+                neighb_1_x, neighb_1_y = i_x-1, i_y + 1
+                neighb_2_x, neighb_2_y = i_x + 1, i_y-1
+              
+            # Now it restarts the cycle
+            elif grad_ang>(22.5 + 135) and grad_ang<=(22.5 + 180):
+                neighb_1_x, neighb_1_y = i_x-1, i_y
+                neighb_2_x, neighb_2_y = i_x + 1, i_y
+               
+            # Non-maximum suppression step
+            if width>neighb_1_x>= 0 and height>neighb_1_y>= 0:
+                if mag[i_y, i_x]<mag[neighb_1_y, neighb_1_x]:
+                    mag[i_y, i_x]= 0
+                    continue
+   
+            if width>neighb_2_x>= 0 and height>neighb_2_y>= 0:
+                if mag[i_y, i_x]<mag[neighb_2_y, neighb_2_x]:
+                    mag[i_y, i_x]= 0
+   
+    weak_ids = np.zeros_like(img)
+    strong_ids = np.zeros_like(img)              
+    ids = np.zeros_like(img)
+       
+    # double thresholding step
+    for i_x in range(width):
+        for i_y in range(height):
+              
+            grad_mag = mag[i_y, i_x]
+              
+            if grad_mag<weak_th:
+                mag[i_y, i_x]= 0
+            elif strong_th>grad_mag>= weak_th:
+                ids[i_y, i_x]= 1
+            else:
+                ids[i_y, i_x]= 2
+       
+       
+    # finally returning the magnitude of
+    # gradients of edges
+    return mag
+
 cv2.imshow('gambar', m)
 # img_histogram_gray_manual(m)
 # newImageGrayscale = img_grayscale_converter(m)
@@ -132,7 +223,7 @@ cv2.imshow('gambar', m)
 # newImageNegative = img_negative_converter(newImageGrayscale)
 # newImageBlackwhite = img_blackwhite_converter(newImageGrayscale)
 # img_histogram_gray_plt(m)
-img_freq_hist(m)
+# img_freq_hist(m)
 # get_pixel(m)
 
 # cv2.imshow('oldimage', m)
@@ -140,6 +231,7 @@ img_freq_hist(m)
 # cv2.imshow('imgGrayscale', newImageGrayscale)
 # cv2.imshow('imgNegativeGrayscale', newImageNegative)
 # cv2.imshow('imgBlackWhite', newImageBlackwhite)
-# cv2.waitKey(0)
+cv2.imshow('canny_images', Canny_detector(m))
+cv2.waitKey(0)
 # cv2.imshow('newimage', newImage)
 # # img.imsave(os.path.join(outputPath, 'newImage.png'), newImage)
